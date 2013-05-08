a74115960062eb79327cdef03153fa99bcda66db
vv <vv@altsoft.biz>
2013-05-08 12:53:09 +0400

    Tomcat Context additions.

--- designer/PlatypusExplorer/src/com/eas/designer/explorer/j2ee/tomcat/Context.java
+++ designer/PlatypusExplorer/src/com/eas/designer/explorer/j2ee/tomcat/Context.java
@@ -7,6 +7,12 @@ package com.eas.designer.explorer.j2ee.tomcat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
 /**
  * Tomcat 7 web application context.
@@ -15,10 +21,34 @@ import java.util.List;
  */
 public class Context {
 
+    public static final String CONTEXT_TAG_NAME = "Context";//NOI18N
+    protected static DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    protected DocumentBuilder builder;
     private Realm realm;
     private List<Resource> resources = new ArrayList<>();
     private String docBase;
     
+    public Context() throws ParserConfigurationException {
+        builder = factory.newDocumentBuilder();
+    }
+    
+    public Document toDocument() {
+        Document doc = builder.newDocument();
+        doc.setXmlStandalone(true);
+        Element rootTag = doc.createElement(CONTEXT_TAG_NAME);
+        doc.appendChild(rootTag);
+        return doc;
+    }
+    
+    public static Context valueOf(Document aDoc) throws Exception {
+        Context ctx = new Context();
+        NodeList projectNl = aDoc.getElementsByTagName(CONTEXT_TAG_NAME);
+        if (projectNl != null && projectNl.getLength() == 1 && projectNl.item(0) instanceof Element) {
+            Element contextTag = (Element) projectNl.item(0);
+            return ctx;
+        }
+        return null;
+    }
     
     /**
      * Gets the Document Base value, representing directory (also known as the Context Root) for this web application.
@@ -74,6 +104,15 @@ public class Context {
      */
     public synchronized void deleteResource(Resource aResource) {
         resources.remove(aResource);
+    }
+    
+    private static Element getElementByName(Element aParent, String aName) {
+        NodeList nl = aParent.getElementsByTagName(aName);
+        if (nl != null && nl.getLength() == 1 && nl.item(0) instanceof Element) {
+            return (Element) nl.item(0);
+        } else {
+            return null;
+        }
     }
     
     /**
