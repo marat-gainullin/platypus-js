81e6c28159389501731ee70780fc1742a4baa681
ab <ab@altsoft.biz>
2013-06-18 17:54:21 +0400

    kkk

---
+++ application/src/components/Forms/src/com/eas/client/forms/api/Ordering.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.eas.client.forms.api;
+
+import javax.swing.JComponent;
+
+/**
+ *
+ * @author AB
+ */
+public class Ordering {
+    
+    public static void toFront(JComponent aParent, Component aComp) {
+        aParent.setComponentZOrder(aComp.delegate, 0);
+        aParent.revalidate();
+        aParent.repaint();
+    }
+
+    public static void toBack(JComponent aParent, Component aComp) {
+        int zOrder = aParent.getComponentCount();
+        aParent.setComponentZOrder(aComp.delegate, --zOrder);
+        aParent.revalidate();
+        aParent.repaint();
+    }
+    public static void toFront(JComponent aParent, Component aComp, int aCount) {
+        int zOrder = aParent.getComponentZOrder(aComp.delegate);
+        if ((zOrder - aCount) >= 0) {
+            aParent.setComponentZOrder(aComp.delegate, zOrder - aCount);
+            aParent.revalidate();
+            aParent.repaint();
+        }
+    }
+
+    public static void toBack(JComponent aParent, Component aComp, int aCount) {
+        int zOrder = aParent.getComponentZOrder(aComp.delegate);
+        if ((zOrder + aCount) < aParent.getComponentCount()) {
+            aParent.setComponentZOrder(aComp.delegate, zOrder + aCount);
+            aParent.revalidate();
+            aParent.repaint();
+        }
+    }
+}
