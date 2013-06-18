c79abf1e9d10618c749409aaa590ece59a618c02
vv <vv@altsoft.biz>
2013-06-18 16:56:01 +0400

    associateLookup(Lookups.fixed(dataObject)) deleted from setDataObject()

--- designer/PlatypusQueries/src/com/eas/designer/application/query/PlatypusQueryView.java
+++ designer/PlatypusQueries/src/com/eas/designer/application/query/PlatypusQueryView.java
@@ -62,7 +62,6 @@ import org.openide.text.NbDocument;
 import org.openide.util.ImageUtilities;
 import org.openide.util.NbBundle;
 import org.openide.util.Utilities;
-import org.openide.util.lookup.Lookups;
 import org.openide.windows.CloneableTopComponent;
 import org.openide.windows.TopComponent;
 import org.openide.windows.TopComponentGroup;
