a421c2a9d107234eb4222c05e3c5384cde173021
mg <mg@altsoft.biz>
2013-08-07 16:31:03 +0400

    Fixed some bugs on events

--- web-client/src/platypus/src/com/eas/client/gxtcontrols/wrappers/component/PlatypusHtmlEditor.java
+++ web-client/src/platypus/src/com/eas/client/gxtcontrols/wrappers/component/PlatypusHtmlEditor.java
@@ -3,12 +3,89 @@ package com.eas.client.gxtcontrols.wrappers.component;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import com.google.gwt.event.dom.client.HasAllKeyHandlers;
+import com.google.gwt.event.dom.client.KeyDownEvent;
+import com.google.gwt.event.dom.client.KeyDownHandler;
+import com.google.gwt.event.dom.client.KeyPressEvent;
+import com.google.gwt.event.dom.client.KeyPressHandler;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.sencha.gxt.core.shared.event.GroupingHandlerRegistration;
+import com.sencha.gxt.widget.core.client.event.BlurEvent;
+import com.sencha.gxt.widget.core.client.event.FocusEvent;
 import com.sencha.gxt.widget.core.client.form.HtmlEditor;
 
-public class PlatypusHtmlEditor extends HtmlEditor {
+public class PlatypusHtmlEditor extends HtmlEditor implements HasAllKeyHandlers {
+	protected class FocusKeyboardManager implements FocusEvent.FocusHandler, BlurEvent.BlurHandler, com.google.gwt.event.dom.client.FocusHandler, com.google.gwt.event.dom.client.BlurHandler,
+	        KeyDownHandler, KeyPressHandler, KeyUpHandler {
+
+		@Override
+		public void onBlur(final BlurEvent event) {
+			PlatypusHtmlEditor.this.fireEvent(event);
+		}
+
+		@Override
+		public void onFocus(final FocusEvent event) {
+			PlatypusHtmlEditor.this.fireEvent(event);
+		}
+
+		@Override
+		public void onBlur(com.google.gwt.event.dom.client.BlurEvent event) {
+			PlatypusHtmlEditor.this.fireEvent(new BlurEvent());
+		}
+
+		@Override
+		public void onFocus(com.google.gwt.event.dom.client.FocusEvent event) {
+			PlatypusHtmlEditor.this.fireEvent(new FocusEvent());
+		}
+
+		@Override
+		public void onKeyUp(KeyUpEvent event) {
+			PlatypusHtmlEditor.this.fireEvent(event);
+		}
+
+		@Override
+		public void onKeyPress(KeyPressEvent event) {
+			PlatypusHtmlEditor.this.fireEvent(event);
+		}
+
+		@Override
+		public void onKeyDown(KeyDownEvent event) {
+			PlatypusHtmlEditor.this.fireEvent(event);
+		}
+	}
+
+	protected FocusKeyboardManager focusKeyboardManager = new FocusKeyboardManager();
+	protected GroupingHandlerRegistration registered = new GroupingHandlerRegistration();
 
 	public PlatypusHtmlEditor() {
 		super();
+		reregisterFocusBlur();
+	}
+
+	protected void reregisterFocusBlur() {
+		registered.removeHandler();
+		if (sourceTextArea != null) {
+			registered.add(sourceTextArea.addFocusHandler(focusKeyboardManager));
+			registered.add(sourceTextArea.addBlurHandler(focusKeyboardManager));
+			registered.add(sourceTextArea.addKeyDownHandler(focusKeyboardManager));
+			registered.add(sourceTextArea.addKeyPressHandler(focusKeyboardManager));
+			registered.add(sourceTextArea.addKeyUpHandler(focusKeyboardManager));
+		}
+		if (textArea != null) {
+			registered.add(textArea.addFocusHandler(focusKeyboardManager));
+			registered.add(textArea.addBlurHandler(focusKeyboardManager));
+			registered.add(textArea.addKeyDownHandler(focusKeyboardManager));
+			registered.add(textArea.addKeyPressHandler(focusKeyboardManager));
+			registered.add(textArea.addKeyUpHandler(focusKeyboardManager));
+		}
+	}
+
+	@Override
+	protected void toggleSourceEditMode() {
+		super.toggleSourceEditMode();
+		reregisterFocusBlur();
 	}
 
 	@Override
@@ -55,6 +132,21 @@ public class PlatypusHtmlEditor extends HtmlEditor {
 				recurse = false;
 			}
 		}
+	}
+
+	@Override
+	public HandlerRegistration addKeyDownHandler(KeyDownHandler handler) {
+		return addDomHandler(handler, KeyDownEvent.getType());
+	}
+
+	@Override
+	public HandlerRegistration addKeyPressHandler(KeyPressHandler handler) {
+		return addDomHandler(handler, KeyPressEvent.getType());
+	}
+
+	@Override
+	public HandlerRegistration addKeyUpHandler(KeyUpHandler handler) {
+		return addDomHandler(handler, KeyUpEvent.getType());
 	}
 
 }
