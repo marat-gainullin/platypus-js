d1be15104ef92f86056c0fe4ee70141fdee95519
vv <vv@altsoft.biz>
2013-11-14 12:05:02 +0400

    JsDocs fixes implemented

--- application/src/components/Forms/src/com/eas/client/forms/api/containers/ButtonGroup.java
+++ application/src/components/Forms/src/com/eas/client/forms/api/containers/ButtonGroup.java
@@ -16,8 +16,8 @@ import com.eas.script.ScriptFunction;
 public class ButtonGroup extends Container<ButtonGroupWrapper> {
 
     private static final String CONSTRUCTOR_JSDOC = "/**\n"
-            + "* Creates a multiple-exclusion scope for a set of buttons."
-            +" Creating a set of buttons with the same <code>ButtonGroup</code> object means that turning \"on\" one of those buttons turns off all other buttons in the group. \n"
+            + "* Creates a multiple-exclusion scope for a set of buttons.\n"
+            +" * Creating a set of buttons with the same <code>ButtonGroup</code> object means that turning \"on\" one of those buttons turns off all other buttons in the group. \n"
             + "*/";
 
     @ScriptFunction(jsDoc = CONSTRUCTOR_JSDOC, params = {})
@@ -31,7 +31,11 @@ public class ButtonGroup extends Container<ButtonGroupWrapper> {
         setDelegate(aDelegate);
     }
     
-    @ScriptFunction(jsDoc = "Appends the specified component to the end of this container.")
+    private static final String ADD_JSDOC = "/**\n"
+            + "* Appends the specified component to the end of this container.\n"
+            + "*/";
+    
+    @ScriptFunction(jsDoc = ADD_JSDOC)
     public void add(Component<?> aComp) {
         if (aComp != null) {
             delegate.add(unwrap(aComp));
