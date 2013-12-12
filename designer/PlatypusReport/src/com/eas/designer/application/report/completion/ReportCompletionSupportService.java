099dc423e4dae79393c819f41809d455cf8cf660
vv <vv@altsoft.biz>
2013-12-12 11:56:19 +0400

    Refactoring

--- designer/PlatypusReport/src/com/eas/designer/application/report/completion/ReportCompletionSupportService.java
+++ designer/PlatypusReport/src/com/eas/designer/application/report/completion/ReportCompletionSupportService.java
@@ -4,14 +4,9 @@
  */
 package com.eas.designer.application.report.completion;
 
-import com.eas.client.cache.PlatypusFiles;
-import com.eas.designer.application.indexer.AppElementInfo;
-import com.eas.designer.application.module.completion.AppElementConstructorCompletionItem;
 import com.eas.designer.application.module.completion.CompletionPoint;
 import com.eas.designer.application.module.completion.CompletionSupportService;
-import com.eas.designer.application.module.completion.JsCompletionProvider;
 import com.eas.designer.application.module.completion.SystemConstructorCompletionItem;
-import com.eas.designer.application.report.PlatypusReportDataLoader;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -26,7 +21,7 @@ import org.openide.util.lookup.ServiceProvider;
 public class ReportCompletionSupportService implements CompletionSupportService {
 
     private static final String REPORT_CONSTRUCTOR_NAME = "Report";//NOI18N
-    private static final String REPORT_CONSTRUCTOR_JSDOC = "/**\n"
+    private static final String REPORT_CONSTRUCTOR_JSDOC = "/**\n"//NOI18N
             + "* Creates new Platypus Report application element instance.\n"//NOI18N
             + "* @param name Report application element name\n"//NOI18N
             + "*/";//NOI18N
