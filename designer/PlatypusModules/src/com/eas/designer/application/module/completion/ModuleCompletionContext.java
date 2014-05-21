e370c367de00f60b64d8982eeec8efdc1741fa11
vv <vv@altsoft.biz>
2014-05-21 11:23:37 +0400

    Unused completion code removed.

--- designer/PlatypusModules/src/com/eas/designer/application/module/completion/ModuleCompletionContext.java
+++ designer/PlatypusModules/src/com/eas/designer/application/module/completion/ModuleCompletionContext.java
@@ -7,14 +7,11 @@ package com.eas.designer.application.module.completion;
 import com.eas.client.model.application.ApplicationDbModel;
 import com.eas.designer.application.indexer.IndexerQuery;
 import com.eas.designer.application.module.PlatypusModuleDataObject;
-import static com.eas.designer.application.module.completion.CompletionContext.addItem;
 import com.eas.designer.application.module.completion.CompletionPoint.CompletionToken;
-import com.eas.designer.explorer.utils.StringUtils;
 import com.eas.script.EventMethod;
 import com.eas.script.ScriptUtils;
 import com.eas.util.PropertiesUtils;
 import java.lang.reflect.Method;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -28,11 +25,9 @@ import jdk.nashorn.internal.ir.FunctionNode;
 import jdk.nashorn.internal.ir.IdentNode;
 import jdk.nashorn.internal.ir.LexicalContext;
 import jdk.nashorn.internal.ir.LiteralNode;
-import jdk.nashorn.internal.ir.Node;
 import jdk.nashorn.internal.ir.UnaryNode;
 import jdk.nashorn.internal.ir.VarNode;
 import jdk.nashorn.internal.ir.visitor.NodeVisitor;
-import jdk.nashorn.internal.parser.Token;
 import jdk.nashorn.internal.parser.TokenType;
 import org.netbeans.api.project.Project;
 import org.netbeans.spi.editor.completion.CompletionResultSet;
@@ -40,7 +35,6 @@ import org.openide.ErrorManager;
 import org.openide.filesystems.FileObject;
 import org.openide.loaders.DataObject;
 import org.openide.loaders.DataObjectNotFoundException;
-import org.openide.util.Lookup;
 
 /**
  *
@@ -88,47 +82,9 @@ public class ModuleCompletionContext extends CompletionContext {
     public void applyCompletionItems(CompletionPoint point, int offset, CompletionResultSet resultSet) throws Exception {
     }
 
-    protected void fillSystemConstructors(CompletionPoint point, CompletionResultSet resultSet) {
-        for (CompletionSupportService scp : Lookup.getDefault().lookupAll(CompletionSupportService.class)) {
-            Collection<JsCompletionItem> items = scp.getSystemConstructors(point);
-            if (items != null) {
-                for (JsCompletionItem item : items) {
-                    addItem(resultSet, point.getFilter(), item);
-                }
-            }
-        }
-    }
-
-    protected void fillSystemObjects(CompletionPoint point, CompletionResultSet resultSet) {
-        for (CompletionSupportService scp : Lookup.getDefault().lookupAll(CompletionSupportService.class)) {
-            Collection<JsCompletionItem> items = scp.getSystemObjects(point);
-            if (items != null) {
-                for (JsCompletionItem item : items) {
-                    addItem(resultSet, point.getFilter(), item);
-                }
-            }
-        }
-    }
-
     @Override
     public CompletionContext getChildContext(CompletionToken token, int offset) throws Exception {
         return findCompletionContext(token.name, offset, this);
-    }
-
-    public static JsCodeCompletionScopeInfo getCompletionScopeInfo(PlatypusModuleDataObject aDataObject, int offset, String text) {
-        FunctionNode tree = aDataObject.getAstRoot();
-        Node offsetNode = ScriptUtils.getOffsetNode(tree, offset);
-        CompletionMode codeCompletionInfo = isInNewExpression(offsetNode, text) ? CompletionMode.CONSTRUCTORS : CompletionMode.VARIABLES_AND_FUNCTIONS;
-        return new JsCodeCompletionScopeInfo(offsetNode, codeCompletionInfo);
-    }
-
-    private static boolean isInNewExpression(jdk.nashorn.internal.ir.Node aNode, String txt) {
-        assert false : "Refactoring is needed";
-        return false;
-        /*
-         return (aNode != null) && ((aNode instanceof NewExpression && (txt == null || txt.isEmpty()))
-         || (aNode instanceof Name && aNode.getParent() instanceof NewExpression));
-         */
     }
 
     public static ModuleCompletionContext getModuleCompletionContext(Project project, String appElementId) {
@@ -320,196 +276,5 @@ public class ModuleCompletionContext extends CompletionContext {
             }
         }
         return null;
-    }
-
-    public enum CompletionMode {
-
-        VARIABLES_AND_FUNCTIONS, CONSTRUCTORS
-    }
-
-    public static class JsCodeCompletionScopeInfo {
-
-        public final jdk.nashorn.internal.ir.Node scope;
-        public final CompletionMode mode;
-
-        public JsCodeCompletionScopeInfo(jdk.nashorn.internal.ir.Node aScope, CompletionMode aMode) {
-            scope = aScope;
-            mode = aMode;
-        }
-    }
-
-    private static class FindModuleElementSupport {
-
-        private final jdk.nashorn.internal.ir.Node moduleConstructorScope;
-        private final jdk.nashorn.internal.ir.Node parentScope;
-        private final jdk.nashorn.internal.ir.Node lookupScope;
-
-        private final String fieldName;
-        private final ModuleCompletionContext parentContext;
-        private CompletionContext ctx;
-
-        public FindModuleElementSupport(jdk.nashorn.internal.ir.Node aModuleConstructor, jdk.nashorn.internal.ir.Node aParentNode, jdk.nashorn.internal.ir.Node aLookupScope, String aFieldName, ModuleCompletionContext aParentContext) {
-            moduleConstructorScope = aModuleConstructor;
-            parentScope = aParentNode;
-            lookupScope = aLookupScope;
-            fieldName = aFieldName;
-            parentContext = aParentContext;
-        }
-
-        public CompletionContext findContext() {
-            assert false : "Refactoring is needed";
-            /*
-             lookupScope.visit(new NodeVisitor() {
-             @Override
-             public boolean visit(AstNode an) {
-             if (an == lookupScope) {
-             if (an instanceof FunctionNode) {
-             FunctionNode fn = (FunctionNode) an;
-             if (fn.getParams() != null && fn.getParams().size() > 0
-             && fieldName.equals(fn.getParams().get(0).toSource())) {// function parameter completion
-             if (fn.getParent() instanceof FunctionCall) { // array iteration methods parameters on an entity with anonymous function an the fist parameter
-             FunctionCall fc = (FunctionCall) fn.getParent();
-             List<CompletionToken> tokens = CompletionPoint.getContextTokens(fc);
-             if (tokens != null && tokens.size() > 1) {
-             CompletionToken funcitonCallToken = tokens.get(tokens.size() - 1);
-             if (fn == fc.getArguments().get(0) && ARRAY_ITERATION_FUNCTIONS_NAMES.contains(funcitonCallToken.name)) {
-             try {
-             CompletionContext c = ModuleCompletionProvider.getCompletionContext(parentContext, tokens.subList(0, tokens.size() - 1), fc.getAbsolutePosition());
-             if (c instanceof EntityCompletionContext) {
-             ctx = ((EntityCompletionContext) c).getElementCompletionContext();
-             return false;
-             }
-             } catch (Exception ex) {
-             ErrorManager.getDefault().notify(ex);
-             }
-             }
-             }
-             } else if (fn.getParent() instanceof Assignment) { // event handler function pameter with event assignment
-             Assignment assignment = (Assignment) fn.getParent();
-             if (assignment.getLeft() instanceof PropertyGet) {
-             PropertyGet leftPg = (PropertyGet) assignment.getLeft();
-             List<CompletionToken> tokens = CompletionPoint.getContextTokens(leftPg);
-             if (tokens != null && tokens.size() > 1) {
-             try {
-             CompletionContext propsCtx = ModuleCompletionProvider.getCompletionContext(parentContext, tokens.subList(0, tokens.size() - 1), leftPg.getAbsolutePosition());
-             if (propsCtx != null && propsCtx.getScriptClass() != null) {
-             Class<?> eventClass = getEventClass(propsCtx.getScriptClass(), tokens.get(tokens.size() - 1).name);
-             if (eventClass != null) {
-             ctx = new CompletionContext(eventClass);
-             return false;
-             }
-             }
-             } catch (Exception ex) {
-             ErrorManager.getDefault().notify(ex);
-             }
-             }
-             }
-             }
-             }
-             }
-             return true;
-             }
-             if (parentScope == moduleConstructorScope) {
-             if (an instanceof PropertyGet) {
-             PropertyGet pg = (PropertyGet) an;
-             if (THIS_KEYWORD.equals(fieldName)
-             && pg.getTarget() instanceof KeywordLiteral
-             && Token.THIS == pg.getTarget().getType()) { // this.prop1
-             ctx = parentContext.createThisContext(false);
-             return false;
-             }
-             } else if (an instanceof ExpressionStatement) {
-             ExpressionStatement es = (ExpressionStatement) an;
-             if (THIS_KEYWORD.equals(fieldName)
-             && es.getExpression() instanceof KeywordLiteral
-             && Token.THIS == es.getExpression().getType()) { // this.
-             ctx = parentContext.createThisContext(false);
-             return false;
-             }
-             } else if (Token.THIS == an.getType() && THIS_KEYWORD.equals(fieldName)) {
-             ctx = parentContext.createThisContext(false);
-             return false;
-             }
-             }
-             if (an instanceof VariableDeclaration) {
-             VariableDeclaration variableDeclaration = (VariableDeclaration) an;
-             if (variableDeclaration.getVariables() != null) {
-             for (VariableInitializer variableInitializer : variableDeclaration.getVariables()) {
-             if (variableInitializer.getTarget() != null
-             && variableInitializer.getTarget().getString().equals(fieldName)
-             && variableInitializer.getInitializer() != null) {
-             if (variableInitializer.getInitializer() instanceof NewExpression) {
-             NewExpression ne = (NewExpression) variableInitializer.getInitializer();
-             if (ne.getTarget() != null && ne.getTarget() instanceof Name) {
-             //checks for new Module(moduleName) like expression 
-             if (isModuleInitializerName(ne.getTarget().getString())
-             && ne.getArguments() != null
-             && ne.getArguments().size() > 0) {
-             ModuleCompletionContext mctx = getModuleCompletionContext(parentContext.getDataObject().getProject(), stripElementId(ne.getArguments().get(0).toSource()));
-             if (mctx != null) {
-             ctx = mctx.createThisContext(true);
-             }
-             return false;
-             }
-             //checks for Platypus API classes
-             for (CompletionSupportService scp : Lookup.getDefault().lookupAll(CompletionSupportService.class)) {
-             Class clazz = scp.getClassByName(ne.getTarget().getString());
-             if (clazz != null) {
-             ctx = new CompletionContext(clazz);
-             return false;
-             }
-             }
-             //checks for new ModuleName() expression
-             ModuleCompletionContext mcc = getModuleCompletionContext(parentContext.getDataObject().getProject(), stripElementId(ne.getTarget().getString()));
-             if (mcc != null) {
-             CompletionContext cc = mcc.createThisContext(true);
-             if (cc != null) {
-             ctx = cc;
-             return false;
-             }
-             }
-             }
-             //checks for Modules.get(moduleName) expression
-             } else if (variableInitializer.getInitializer() instanceof FunctionCall) {
-             FunctionCall fc = (FunctionCall) variableInitializer.getInitializer();
-             if (fc.getTarget() instanceof PropertyGet) {
-             PropertyGet pg = (PropertyGet) fc.getTarget();
-             if (pg.getLeft().getString().equals(MODULES_OBJECT_NAME)
-             && pg.getRight().getString().equals(GET_METHOD_NAME)) {
-             if (fc.getArguments() != null && fc.getArguments().size() > 0) {
-             ctx = getModuleCompletionContext(parentContext.getDataObject().getProject(), stripElementId(fc.getArguments().get(0).toSource())).createThisContext(true);
-             return false;
-             }
-             }
-             }
-             } else if (variableInitializer.getInitializer() instanceof KeywordLiteral
-             && Token.THIS == variableInitializer.getInitializer().getType()) {// var self = this;
-             ctx = parentContext.createThisContext(false);
-             return false;
-             } else {
-             List<CompletionToken> tokens = CompletionPoint.getContextTokens(variableInitializer);
-             if (tokens != null && tokens.size() > 1) {
-             try {
-             ctx = ModuleCompletionProvider.getCompletionContext(parentContext, tokens, variableInitializer.getAbsolutePosition());
-             return false;
-             } catch (Exception ex) {
-             ErrorManager.getDefault().notify(ex);
-             }
-             }
-             }
-             }
-             }
-             }
-             }
-             return true;
-             }
-             });
-             */
-            return ctx;
-        }
-
-        private static String stripElementId(String str) {
-            return StringUtils.strip(StringUtils.strip(StringUtils.strip(str, "\""), "'"));//NOI18N
-        }
     }
 }
