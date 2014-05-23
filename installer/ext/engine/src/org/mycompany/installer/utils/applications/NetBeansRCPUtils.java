1f59179edf2ea9c891c9cac626a1f03a103911fa
vv <vv@altsoft.biz>
2014-05-23 15:34:01 +0400

    Uninstaller fix.

--- installer/ext/engine/src/org/mycompany/installer/utils/applications/NetBeansRCPUtils.java
+++ installer/ext/engine/src/org/mycompany/installer/utils/applications/NetBeansRCPUtils.java
@@ -68,24 +68,28 @@ public class NetBeansRCPUtils {
      */
     public static File getApplicationUserDirFile(File appLocation) throws IOException {
         String dir = getApplicationUserDir(appLocation);
-        String userHome = System.getProperty("user.home");
-        if(SystemUtils.isWindows()) {
-            WindowsNativeUtils wnu = (WindowsNativeUtils) SystemUtils.getNativeUtils();
-            WindowsRegistry reg = wnu.getWindowsRegistry();
-            String key = "Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Shell Folders";
-            try {
-            if(reg.keyExists(reg.HKCU, key) &&
-                    reg.valueExists(reg.HKCU, key, "AppData")) {
-                userHome = reg.getStringValue(reg.HKCU, key, "AppData", false);
+	if (dir != null) {
+		String userHome = System.getProperty("user.home");
+		if(SystemUtils.isWindows()) {
+		    WindowsNativeUtils wnu = (WindowsNativeUtils) SystemUtils.getNativeUtils();
+		    WindowsRegistry reg = wnu.getWindowsRegistry();
+		    String key = "Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Shell Folders";
+		    try {
+		    if(reg.keyExists(reg.HKCU, key) &&
+		            reg.valueExists(reg.HKCU, key, "AppData")) {
+		        userHome = reg.getStringValue(reg.HKCU, key, "AppData", false);
 
-            }
-            } catch (NativeException e) {
-                LogManager.log(e);
-            }
-        }
-        dir = dir.replace(USER_HOME_TOKEN, userHome);
-        dir = dir.replace(APPNAME_TOKEN, getApplicationName(appLocation));
-        return new File(dir);
+		    }
+		    } catch (NativeException e) {
+		        LogManager.log(e);
+		    }
+		}
+		dir = dir.replace(USER_HOME_TOKEN, userHome);
+		dir = dir.replace(APPNAME_TOKEN, getApplicationName(appLocation));
+		return new File(dir);
+	} else {
+		return null;	
+	}
     }
     
     /**
