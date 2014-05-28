78e4b892da30e61ce40472b87420c43ffee5697b
vv <vv@altsoft.biz>
2014-05-28 15:35:29 +0400

    Publisher support implemented.

--- application/src/lib/own/Grid/src/com/bearsoft/gui/grid/data/CellData.java
+++ application/src/lib/own/Grid/src/com/bearsoft/gui/grid/data/CellData.java
@@ -5,23 +5,31 @@
 package com.bearsoft.gui.grid.data;
 
 import com.eas.gui.CascadedStyle;
+import com.eas.script.AlreadyPublishedException;
+import com.eas.script.HasPublished;
+import com.eas.script.NoPublisherException;
 import com.eas.script.ScriptFunction;
-
+import jdk.nashorn.api.scripting.JSObject;
 
 /**
  * Class, aggregating row data, style information and substituting information,
  * that should be displayed instead of real data.
+ *
  * @author Gala
  */
-public class CellData implements Comparable<Object> {
+public class CellData implements Comparable<Object>, HasPublished  {
 
     public CascadedStyle style = null;
     public Object data = null;
     public Object display = null;
 
+    protected static JSObject publisher;
+    protected Object published;
     /**
      * Simple constructor for controls models data.
-     * @param aStyle CascadedStyle instance, holding information about style of the cell, row or other unit of a control.
+     *
+     * @param aStyle CascadedStyle instance, holding information about style of
+     * the cell, row or other unit of a control.
      * @param aData Real data, took from subject area data.
      * @param aDisplay Data, thet should be displayed instead of real data
      * @see CascadedStyle
@@ -35,37 +43,40 @@ public class CellData implements Comparable<Object> {
 
     /**
      * Returns CascadedStyle instance of this data unit.
+     *
      * @return CascadedStyle instance.
      * @see CascadedStyle
      */
     @ScriptFunction(jsDoc = ""
-                + "/**\n"
-                + " * The cell's style.\n"
-                + " */")
+            + "/**\n"
+            + " * The cell's style.\n"
+            + " */")
     public CascadedStyle getStyle() {
         return style;
     }
 
     /**
      * Returns real data from subject area.
+     *
      * @return Real data.
      */
     @ScriptFunction(jsDoc = ""
-                + "/**\n"
-                + " * The cell's data.\n"
-                + " */")
+            + "/**\n"
+            + " * The cell's data.\n"
+            + " */")
     public Object getData() {
         return data;
     }
 
     /**
      * Returns data, that should be displayed instead of rela data.
+     *
      * @return
      */
     @ScriptFunction(jsDoc = ""
-                + "/**\n"
-                + " * The displayed text.\n"
-                + " */")
+            + "/**\n"
+            + " * The displayed text.\n"
+            + " */")
     public Object getDisplay() {
         return display;
     }
@@ -87,5 +98,28 @@ public class CellData implements Comparable<Object> {
             }
         }
         return 0;
+    }
+
+    @Override
+    public Object getPublished() {
+        if (published == null) {
+            if (publisher == null || !publisher.isFunction()) {
+                throw new NoPublisherException();
+            }
+            published = publisher.call(null, new Object[]{});
+        }
+        return published;
+    }
+
+    @Override
+    public void setPublished(Object aValue) {
+        if (published != null) {
+            throw new AlreadyPublishedException();
+        }
+        published = aValue;
+    }
+
+    public static void setPublisher(JSObject aPublisher) {
+        publisher = aPublisher;
     }
 }
