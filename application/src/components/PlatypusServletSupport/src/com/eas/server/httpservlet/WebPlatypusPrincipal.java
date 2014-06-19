5a6e540e3d5a8add313ee2473783ad5501f8bae4
ab <ab@altsoft.biz>
2014-06-19 17:10:34 +0400

    Fixed jhttp script context.

--- application/src/components/PlatypusServletSupport/src/com/eas/server/httpservlet/WebPlatypusPrincipal.java
+++ application/src/components/PlatypusServletSupport/src/com/eas/server/httpservlet/WebPlatypusPrincipal.java
@@ -6,6 +6,9 @@ package com.eas.server.httpservlet;
 
 import com.eas.client.login.PlatypusPrincipal;
 import com.eas.script.NoPublisherException;
+import com.eas.server.PlatypusServerCore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 import jdk.nashorn.api.scripting.JSObject;
 
@@ -15,18 +18,24 @@ import jdk.nashorn.api.scripting.JSObject;
  */
 public class WebPlatypusPrincipal extends PlatypusPrincipal {
 
-    protected PlatypusHttpServlet servlet;
+    protected PlatypusServerCore core;
 
-    public WebPlatypusPrincipal(String aUserName, PlatypusHttpServlet aServlet) {
+    public WebPlatypusPrincipal(String aUserName, PlatypusServerCore aServerCore) {
         super(aUserName);
-        servlet = aServlet;
+        core = aServerCore;
     }
 
     @Override
     public boolean hasRole(String aRole) throws Exception {
-        HttpServletRequest currentRequest = servlet.getCurrentRequest();
-        assert currentRequest != null : "Current request is null"; //NOI18N
-        return currentRequest.isUserInRole(aRole) || servlet.getServerCore().isUserInApplicationRole(getName(), aRole);
+        Object request = core.getCurrentRequest().get();
+        if (request instanceof HttpServletRequest) {
+            HttpServletRequest currentRequest = (HttpServletRequest) request;
+            assert currentRequest != null : "Current request is null"; //NOI18N
+            return currentRequest.isUserInRole(aRole) || core.isUserInApplicationRole(getName(), aRole);
+        } else {
+            Logger.getLogger(WebPlatypusPrincipal.class.getName()).log(Level.WARNING, "Bad request type.");
+            return false;
+        }
     }
 
     @Override
