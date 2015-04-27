51708d408966b1864580fd6220fa5564d70b3dfe
mg <mg@altsoft.biz>
2015-04-27 17:35:49 +0300

    fr

--- designer/PlatypusModules/src/com/eas/designer/application/module/completion/EntityElementCompletionContext.java
+++ designer/PlatypusModules/src/com/eas/designer/application/module/completion/EntityElementCompletionContext.java
@@ -8,6 +8,7 @@ import com.eas.client.metadata.Field;
 import com.eas.client.metadata.Fields;
 import com.eas.client.model.application.ApplicationDbEntity;
 import com.eas.client.model.application.ReferenceRelation;
+import com.eas.client.model.Relation;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
