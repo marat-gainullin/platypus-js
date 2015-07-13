1d3f68062ae70514e7c180e370fe1a3d139847d6
Your Name <you@example.com>
2015-07-13 11:32:27 +0300

    formatted field types changes

--- web-client/src/platypus/src/com/eas/client/form/published/widgets/PlatypusFormattedTextField.java
+++ web-client/src/platypus/src/com/eas/client/form/published/widgets/PlatypusFormattedTextField.java
@@ -233,10 +233,31 @@ public class PlatypusFormattedTextField extends FormattedObjectBox implements Ha
 		});
 		Object.defineProperty(published, "valueType", {
 			get : function() {
-				return $wnd.P.boxAsJs(aWidget.@com.eas.client.form.published.widgets.PlatypusFormattedTextField::getValueType()());
+				var typeNum = aWidget.@com.eas.client.form.published.widgets.PlatypusFormattedTextField::getValueType()()
+				var type;
+				if (typeNum === @com.bearsoft.gwt.ui.widgets.ObjectFormat::NUMBER ){
+					type = $wnd.Number;
+				} else if (typeNum === @com.bearsoft.gwt.ui.widgets.ObjectFormat::DATE ){
+					type = $wnd.Date;
+				} else if (typeNum === @com.bearsoft.gwt.ui.widgets.ObjectFormat::REGEXP ){
+					type = $wnd.RegExp;
+				} else {
+					type = $wnd.String;
+				}
+				return type;
 			},
 			set : function(aValue) {
-				aWidget.@com.eas.client.form.published.widgets.PlatypusFormattedTextField::setValueType(I)(aValue != null ? aValue : null);
+				var typeNum;
+				if (aValue === $wnd.Number ){
+					typeNum = @com.bearsoft.gwt.ui.widgets.ObjectFormat::NUMBER;
+				} else if (aValue === $wnd.Date ){
+					typeNum = @com.bearsoft.gwt.ui.widgets.ObjectFormat::DATE;
+				} else if (aValue === $wnd.RegExp ){
+					typeNum = @com.bearsoft.gwt.ui.widgets.ObjectFormat::REGEXP;
+				} else {
+					typeNum = @com.bearsoft.gwt.ui.widgets.ObjectFormat::TEXT;
+				}
+				aWidget.@com.eas.client.form.published.widgets.PlatypusFormattedTextField::setValueType(I)(typeNum);
 			}
 		});
 		
