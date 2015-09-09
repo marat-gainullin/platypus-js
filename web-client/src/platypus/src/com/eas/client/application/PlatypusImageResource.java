e171d79a56d05bd71733de84e0286cce26859d8d
mg <mg@altsoft.biz>
2015-09-09 18:18:40 +0300

    fix P.Icon.load in browser client now do not caching

--- web-client/src/platypus/src/com/eas/client/application/PlatypusImageResource.java
+++ web-client/src/platypus/src/com/eas/client/application/PlatypusImageResource.java
@@ -1,7 +1,5 @@
 package com.eas.client.application;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -25,7 +23,6 @@ public class PlatypusImageResource implements ImageResource {
 	protected String name;
 	protected int width;
 	protected int height;
-	private static Map<String, ImageResource> iconsCache = new HashMap<String, ImageResource>();
 
 	public PlatypusImageResource(String aName, int aWidth, int aHeight) {
 		super();
@@ -36,7 +33,7 @@ public class PlatypusImageResource implements ImageResource {
 
 	public static void jsLoad(final String aResourceName, final JavaScriptObject aOnSuccess, final JavaScriptObject aOnFailure) {
 		final String callerDir = Utils.lookupCallerJsDir();
-		String normalizedResourceName = aResourceName.startsWith("./") || aResourceName.startsWith("../") ? AppClient.toAppModuleId(aResourceName, callerDir) : aResourceName; 
+		String normalizedResourceName = aResourceName.startsWith("./") || aResourceName.startsWith("../") ? AppClient.toAppModuleId(aResourceName, callerDir) : aResourceName;
 		load(normalizedResourceName, new CallbackAdapter<ImageResource, String>() {
 
 			@Override
@@ -62,42 +59,34 @@ public class PlatypusImageResource implements ImageResource {
 
 	public static void load(final String aName, final Callback<ImageResource, String> aCallback) {
 		if (aName != null && !aName.isEmpty()) {
-			if (iconsCache.containsKey(aName)) {
-				ImageResource loaded = iconsCache.get(aName);
-				if (aCallback != null) {
-					aCallback.onSuccess(loaded);
+			SafeUri imageUri = AppClient.getInstance().getResourceUri(aName);
+			final Image im = new Image(imageUri);
+			im.addLoadHandler(new LoadHandler() {
+				@Override
+				public void onLoad(LoadEvent event) {
+					int imWidth = im.getWidth();
+					int imHeight = im.getHeight();
+					im.removeFromParent();
+					ImageResource loaded = new PlatypusImageResource(aName, imWidth, imHeight);
+					if (aCallback != null) {
+						aCallback.onSuccess(loaded);
+					}
 				}
-			} else {
-				SafeUri imageUri = AppClient.getInstance().getResourceUri(aName);
-				final Image im = new Image(imageUri);
-				im.addLoadHandler(new LoadHandler() {
-					@Override
-					public void onLoad(LoadEvent event) {
-						int imWidth = im.getWidth();
-						int imHeight = im.getHeight();
-						im.removeFromParent();
-						ImageResource loaded = new PlatypusImageResource(aName, imWidth, imHeight);
-						iconsCache.put(aName, loaded);
-						if (aCallback != null) {
-							aCallback.onSuccess(loaded);
-						}
+			});
+			im.addErrorHandler(new ErrorHandler() {
+				@Override
+				public void onError(ErrorEvent event) {
+					im.removeFromParent();
+					if (aCallback != null) {
+						aCallback.onFailure("Error while loading an image " + aName);
 					}
-				});
-				im.addErrorHandler(new ErrorHandler() {
-					@Override
-					public void onError(ErrorEvent event) {
-						im.removeFromParent();
-						if (aCallback != null) {
-							aCallback.onFailure("Error while loading an image " + aName);
-						}
-					}
-				});
-				im.setVisible(false);
-				im.getElement().getStyle().setPosition(Position.ABSOLUTE);
-				im.getElement().getStyle().setLeft(-Integer.MAX_VALUE / 2, Style.Unit.PX);
-				im.getElement().getStyle().setTop(-Integer.MAX_VALUE / 2, Style.Unit.PX);
-				RootPanel.get().add(im);
-			}
+				}
+			});
+			im.setVisible(false);
+			im.getElement().getStyle().setPosition(Position.ABSOLUTE);
+			im.getElement().getStyle().setLeft(-Integer.MAX_VALUE / 2, Style.Unit.PX);
+			im.getElement().getStyle().setTop(-Integer.MAX_VALUE / 2, Style.Unit.PX);
+			RootPanel.get().add(im);
 		}
 	}
 
