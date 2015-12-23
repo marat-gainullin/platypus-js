64a8ba6a29fa12dabab5a50e00a63819f5c092fe
mg <mg@altsoft.biz>
2015-12-23 18:28:09 +0300

    requirejs plugin support in platypus project

--- designer/PlatypusForms/src/com/bearsoft/org/netbeans/modules/form/docs/package-info.java
+++
@@ -1,8 +0,0 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-@HelpSetRegistration(helpSet = "form-hs.xml", position = 3455)
-package com.bearsoft.org.netbeans.modules.form.docs;
-
-import org.netbeans.api.javahelp.HelpSetRegistration;
--- designer/PlatypusReport/src/com/eas/designer/application/report/docs/package-info.java
+++
@@ -1,8 +0,0 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-@HelpSetRegistration(helpSet = "report-hs.xml", position = 3046)
-package com.eas.designer.application.report.docs;
-
-import org.netbeans.api.javahelp.HelpSetRegistration;
