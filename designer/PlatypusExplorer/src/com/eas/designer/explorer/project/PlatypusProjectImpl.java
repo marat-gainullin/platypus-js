9ed828516d052ec79b208aa0d567fbdaa4f5ee3b
mg <mg@altsoft.biz>
2016-04-08 10:27:54 +0300

    MultiParentClassLoader added. Failed to achieve any further results.

--- designer/PlatypusExplorer/src/com/eas/designer/explorer/project/PlatypusProjectImpl.java
+++ designer/PlatypusExplorer/src/com/eas/designer/explorer/project/PlatypusProjectImpl.java
@@ -391,17 +391,17 @@ public class PlatypusProjectImpl implements PlatypusProject {
         final DatabaseConnection conn = DatabaseConnections.lookup(aDatasourceName);
         assert conn != null : "Already connected datasource disappeared";
         String datasourceName = (aDatasourceName == null ? basesProxy.getDefaultDatasourceName() != null : !aDatasourceName.equals(basesProxy.getDefaultDatasourceName())) ? aDatasourceName : null;
-//        ClassLoader nativeClassLoader = Thread.currentThread().getContextClassLoader();
-//        Thread.currentThread().setContextClassLoader(new org.netbeans.ProxyClassLoader(new ClassLoader[]{nativeClassLoader, conn.getJDBCConnection().getClass().getClassLoader()}, true));
-//        try {
-        MetadataCache mdCache = basesProxy.getMetadataCache(datasourceName);
-        if (mdCache != null) {
-            mdCache.clear();
-            mdCache.fillTablesCacheByConnectionSchema();
+        ClassLoader nativeClassLoader = Thread.currentThread().getContextClassLoader();
+        Thread.currentThread().setContextClassLoader(new MultiParentClassLoader(new URL[]{}, new ClassLoader[]{nativeClassLoader, conn.getJDBCConnection().getClass().getClassLoader()}));
+        try {
+            MetadataCache mdCache = basesProxy.getMetadataCache(datasourceName);
+            if (mdCache != null) {
+                mdCache.clear();
+                mdCache.fillTablesCacheByConnectionSchema();
+            }
+        } finally {
+            Thread.currentThread().setContextClassLoader(nativeClassLoader);
         }
-//        } finally {
-//            Thread.currentThread().setContextClassLoader(nativeClassLoader);
-//        }
 
         String dbConnectingCompleteMsg = NbBundle.getMessage(PlatypusProjectImpl.class, "LBL_Connecting_Complete", aDatasourceName); // NOI18N
         StatusDisplayer.getDefault().setStatusText(dbConnectingCompleteMsg);
