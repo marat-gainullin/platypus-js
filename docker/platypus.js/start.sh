db271f177b2729be85960cccc7c7ea4b1935e6bb
jskonst <jskonst@yandex.ru>
2016-11-17 09:59:03 +0300

    xfb test

--- docker/platypus.js/start.sh
+++ docker/platypus.js/start.sh
@@ -16,5 +16,6 @@ find /platypus/application/bin /platypus/application/ext /platypus/application/l
 # start tomcat server
 /usr/local/apache-tomcat-$TOMCAT_VERSION/bin/startup.sh
 
+Xvfb :1 -screen 0 640x480x24 -fbdir /var/tmp &
 # run TSA server
 java -D.level=INFO -Dhandlers=java.util.logging.ConsoleHandler -Djava.util.logging.ConsoleHandler.level=INFO -Djava.util.logging.ConsoleHandler.formatter=com.eas.util.logging.PlatypusFormatter -Djava.util.logging.config.class=com.eas.util.logging.LoggersConfig -cp /platypus/application/api:/platypus/application/bin/Server.jar com.eas.server.ServerMain -datasource eas -dburl jdbc:mysql://testDb:3306/eas -dbuser test -dbpassword test -dbschema EAS -datasource easHR -dburl jdbc:mysql://testDb:3306/hr -dbuser test -dbpassword test -dbschema hr -default-datasource eas -source-path app -global-api -appelement start.js -url file:///platypusTests/ -iface 0.0.0.0:8500 -protocols 8500:platypus
